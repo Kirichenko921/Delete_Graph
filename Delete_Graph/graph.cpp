0a7996acc33d338b4508e0c5d71b9585fc2c11ac
Kirichenko Alexander <050780@inbox.ru>
2021-05-15 12:13:18 +0300

    Добавьте файлы проекта.

---
+++ Delete_Graph/graph.cpp
@@ -0,0 +1,58 @@
+﻿#include "graph.h"
+#include <iostream>
+
+
+Graph::Graph() {
+    for (int i = 0; i < SIZE; i++)
+        for (int j = 0; j < SIZE; j++)
+            matrix[i][j] = 0;
+    vertexCount = 0;
+}
+// добавление вершины
+void Graph::addVertex(int vnumber)
+{
+    vertexes[vertexCount] = vnumber;
+    vertexCount++;
+}
+// добавление ребра
+void Graph::addEdge(int v1, int v2, int weight)
+{
+    matrix[v1][v2] = weight;
+    matrix[v2][v1] = weight;
+}
+/* Для проверки, существует ли ребро, нужно посмотреть значение элемента матрицы смежности с индексами номеров вершин (если есть ребро, он отличен от 0).*/
+bool Graph::edgeExists(int v1, int v2)
+{
+    return matrix[v1][v2] > 0;
+}
+/*Для проверки, существует ли вершина, нужно проверить, есть ли ее номер в массиве vertexes.*/
+bool Graph::vertexExists(int vnumber)
+{
+    for (int i = 0; i < vertexCount; i++)
+        if (vertexes[i] == vnumber)
+            return true;
+    return false;
+}
+
+void Graph::depth(int start)
+{
+    bool visited[SIZE]; // список посещенных вершин
+    for (int i = 0; i < SIZE; i++)
+        visited[i] = false; // инициализируем как непосещенные
+    depthInner(start, visited); // запуск алгоритма 
+
+    std::cout << std::endl;
+}
+
+void Graph::depthInner(int current, bool visited[])
+{
+    std::cout << "v" << current << " -> "; // вывод текущей
+    visited[current] = true; // помечаем как посещенную
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (edgeExists(current, i) && !visited[i])
+            depthInner(i, visited); // если существует ребро и вершина не посещалась, то пройдем по нему в смежную вершину
+
+    }
+}
+
---
+++ Delete_Graph/task.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include"graph.h"
+
+
+// удаление вершины
+void Graph::delVertex(int vnumber) {
+    // Ваш код должен быть здесь 
+    for (int i = 0; i < vertexCount; ++i)
+    {
+        if (vertexes[i] == vnumber)
+        {
+            vertexes[i] = -1;
+            for(int j = 0; j < vertexCount;++j )
+            {
+                matrix[j][i] = 0;
+                matrix[i][j] = 0;
+            }
+            for (; i < vertexCount; ++i)
+            {
+                vertexes[i] = vertexes[i + 1];
+            }
+            
+            vertexCount--;
+        }
+    }
+}
+// удаление ребра
+void Graph::delEdge(int v1, int v2) {
+    // Ваш код должен быть здесь    
+    matrix[v1][v2] = 0;
+    matrix[v2][v1] = 0;
+}
+
+
+int main()
+{
+   /* Graph g;
+    g.addVertex(0);
+    g.addVertex(1);
+    g.addVertex(2);
+    g.addVertex(3);
+
+    g.addEdge(0, 1, 5);
+    g.addEdge(0, 2, 4);
+    g.addEdge(0, 3, 3);
+    g.addEdge(1, 2, 11);
+    g.addEdge(2, 3, 9);
+
+
+    for (int i = 0; i < SIZE; ++i)
+    {
+            std::cout << g.vertexes[i]<<std::endl;
+        
+    }
+
+    g.delEdge(0, 2);
+    g.delEdge(1, 2);
+    g.delEdge(2, 3);
+    g.delVertex(2);
+
+
+    for (int i = 0; i < SIZE; ++i)
+    {
+        std::cout << g.vertexes[i] << std::endl;
+
+    }*/
+    Graph g;
+    g.addVertex(0);
+    g.addVertex(1);
+    g.addVertex(2);
+    g.addVertex(3);
+    g.addVertex(4);
+    g.addVertex(5);
+    g.addVertex(6);
+    g.addVertex(7);
+    g.addVertex(8);
+
+    g.addEdge(0, 1, 1);
+    g.addEdge(0, 2, 1);
+    g.addEdge(0, 3, 1);
+    g.addEdge(2, 5, 1);
+    g.addEdge(1, 4, 1);
+    g.addEdge(4, 3, 1);
+    g.addEdge(5, 3, 1);
+    g.addEdge(4, 7, 1);
+    g.addEdge(3, 6, 1);
+    g.addEdge(3, 7, 1);
+    g.addEdge(3, 8, 1);
+    g.addEdge(5, 8, 1);
+    g.addEdge(8, 6, 1);
+    g.addEdge(7, 6, 1);
+
+    g.depth(0);
+    return 0;
+}
